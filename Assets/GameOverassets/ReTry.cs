c4c6fcd23f16c4973699d496c1f5936222b30f4d
강도진 <sdh240401@sdh.hs.kr>
2024-11-24 20:41:45 +0900

    end

---
+++ Assets/GameOverassets/ReTry.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Gamescene : MonoBehaviour
+{
+    public void GameSenesCtrl()
+    {
+        SceneManager.LoadScene(0);// 어떤씬 이름으로 이동 할건지
+        
+    }
+
+}
